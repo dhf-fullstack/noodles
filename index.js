affa1c2519299e685f1ff993ae7054e94cd8fdfc
David H. Friedman <david.h.friedman.nyc@gmail.com>
2019-07-25 10:27:24 -0400

    mask canvas for converting click pixel coords to hex row col; click subscription; source marker for all except C bend. todo that, and the missing K piece. next step - generate a puzzle, and respond to clicks by rotating.

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ let c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+let mask = document.createElement('canvas')
+let m = mask.getContext('2d')
+mask.width = window.innerWidth
+mask.height = window.innerHeight
+
 const COLORS = {
   BACKGROUND: '#FEFEF3',
   HEXES: '#F3F1E9',
@@ -11,7 +16,7 @@ const COLORS = {
   SOURCE: '#F4F0EA',
 }
 
-const GRID_HEIGHT = 10
+const GRID_HEIGHT = 8 // for tests 11
 const GRID_WIDTH = 8
 
 const R = 42 //35
@@ -26,38 +31,111 @@ const lineWidth = 16
 const HEX_X0 = Math.floor(RX / 2)
 const HEX_Y0 = 3 * RY
 
-const renderHex = (c, x, y, r) => {
+const cssHex = n => {
+  return `#${('000000' + n.toString(16)).slice(-6)}`
+}
+
+const renderHex = (ctx, x, y, r, color) => {
   const dx = Math.floor(r * Math.cos(Math.PI / 3))
   const dy = Math.floor(r * Math.sin(Math.PI / 3))
   const x1 = x - dx
   const x2 = x + dx
   const y1 = y - dy
   const y2 = y + dy
-  c.beginPath()
-  c.moveTo(x1, y1)
-  c.lineTo(x2, y1)
-  c.lineTo(x + r, y)
-  c.lineTo(x2, y2)
-  c.lineTo(x1, y2)
-  c.lineTo(x - r, y)
-  c.closePath()
-  c.fillStyle = COLORS.HEXES
-  c.fill()
-  //c.stroke()
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y1)
+  ctx.lineTo(x + r, y)
+  ctx.lineTo(x2, y2)
+  ctx.lineTo(x1, y2)
+  ctx.lineTo(x - r, y)
+  ctx.closePath()
+  if (color === undefined) {
+    ctx.fillStyle = COLORS.HEXES
+  } else {
+    ctx.fillStyle = cssHex(color)
+  }
+  ctx.fill()
+  //ctx.stroke()
+}
+
+const pixelCoordsToHexIndex = (x, y) => {
+  // returns [row, col]
+  let c = m.getImageData(x, y, 1, 1).data
+  let h = (c[0] * 256 + c[1]) * 256 + c[2]
+  if (h >= GRID_WIDTH * GRID_HEIGHT) {
+    /* out of bound should always be #ffffff === 16777215 
+       but in testing occasionally got unexpected values.
+       I don't know if the canvas is doing some color blending
+       at edges or if I have a bug.
+       Either way, this hack solves it, since the hexes
+       are colored [0..((w*h)-1]  */
+    return undefined
+  } else {
+    let row = Math.floor(h / GRID_WIDTH)
+    let col = h % GRID_WIDTH
+    return [row, col]
+  }
+}
+
+const canvasClickSubscribers = []
+
+const subscribeToCanvasClick = f => {
+  canvasClickSubscribers.push(f)
+  return () => {
+    let i = canvasClickSubscribers.findIndex(e => e === f)
+    if (i >= 0) {
+      canvasClickSubscribers.splice(i, 1)
+    }
+  }
+}
+
+canvas.onclick = ev => {
+  canvasClickSubscribers.forEach(s => s(ev))
+}
+
+// testing
+// subscribeToCanvasClick(ev => console.log(pixelCoordsToHexIndex(ev.x, ev.y)))
+
+const unsubscribeGridClick = subscribeToCanvasClick(ev =>
+  gridOnClick(ev.x, ev.y)
+)
+
+const gridOnClick = (x, y) => {
+  let coords = pixelCoordsToHexIndex(x, y)
+  if (coords != undefined) {
+    const [row, col] = coords
+    let [x, y] = hexIndexToCenterCoords(row, col)
+    renderHex(c, HEX_X0 + x, HEX_Y0 + y, r, '#ff0000')
+  }
 }
 
 const hexIndexToCenterCoords = (row, col) => {
+  // returns [x, y]
   const dx = R + Math.floor(R * Math.sin(Math.PI / 6))
   const dy = Math.floor(R * Math.cos(Math.PI / 6))
   return [col * dx, row * 2 * dy + (col % 2 ? 0 : dy)]
 }
 
-const renderHexGrid = (c, x0, y0, R, r) => {
+const renderHexGrid = (ctx, x0, y0, R, r) => {
   // outer hexagons of radius R for spacing
   for (let row = 0; row < GRID_HEIGHT; row++) {
     for (let col = 0; col < GRID_WIDTH; col++) {
       let [x, y] = hexIndexToCenterCoords(row, col)
-      renderHex(c, x + x0, y + y0, r)
+      renderHex(ctx, x + x0, y + y0, r)
+    }
+  }
+}
+
+const renderMaskGrid = (ctx, x0, y0, R, r) => {
+  // outer hexagons of radius R for spacing
+  // note: 1000 + k1000 is pretty!
+  let color = 0
+  for (let row = 0; row < GRID_HEIGHT; row++) {
+    for (let col = 0; col < GRID_WIDTH; col++) {
+      let [x, y] = hexIndexToCenterCoords(row, col)
+      color = row * GRID_WIDTH + col
+      renderHex(ctx, x + x0, y + y0, r, color)
     }
   }
 }
@@ -67,6 +145,20 @@ c.rect(1, 1, W, H)
 c.stroke()
 c.fill()
 renderHexGrid(c, HEX_X0, HEX_Y0, R, r)
+
+m.fillStyle = '#FFFFFF'
+m.rect(1, 1, W, H)
+m.stroke()
+m.fill()
+renderMaskGrid(m, HEX_X0, HEX_Y0, R, r)
+
+/* debugging occasional unexpected values on click
+c.fillStyle = '#FFFFFF'
+c.rect(1, 1, W, H)
+c.stroke()
+c.fill()
+renderMaskGrid(c, HEX_X0, HEX_Y0, R, r)
+*/
 
 /*
 c.font = '10px serif'
@@ -108,6 +200,9 @@ const renderTerminus = (x, y, orientation, activated, source) => {
   c.rect(-lineWidth / 2, 0, lineWidth, h)
   c.fill()
   c.restore()
+  if (source) {
+    renderSourceMarker(x, y, orientation)
+  }
 }
 
 const renderBend = (x, y, orientation, activated, source) => {
@@ -127,6 +222,9 @@ const renderBend = (x, y, orientation, activated, source) => {
   c.lineTo(0, cos)
   c.stroke()
   c.restore()
+  if (source) {
+    renderSourceMarker(x, y, orientation)
+  }
 }
 
 const renderStraight = (x, y, orientation, activated, source) => {
@@ -143,6 +241,9 @@ const renderStraight = (x, y, orientation, activated, source) => {
   c.lineTo(0, -cos)
   c.stroke()
   c.restore()
+  if (source) {
+    renderSourceMarker(x, y, orientation)
+  }
 }
 
 const renderCurve = (x, y, orientation, activated, source) => {
@@ -158,6 +259,9 @@ const renderCurve = (x, y, orientation, activated, source) => {
   c.arc(-sin, cos, sin, 0, (-Math.PI * 2) / 3, true)
   c.stroke()
   c.restore()
+  if (source) {
+    renderSourceMarker(x, y, orientation, true)
+  }
 }
 
 const renderSha = (x, y, orientation, activated, source) => {
@@ -181,6 +285,9 @@ const renderSha = (x, y, orientation, activated, source) => {
   c.lineTo(0, cos)
   c.stroke()
   c.restore()
+  if (source) {
+    renderSourceMarker(x, y, orientation)
+  }
 }
 
 const renderTriskelion = (x, y, orientation, activated, source) => {
@@ -203,6 +310,9 @@ const renderTriskelion = (x, y, orientation, activated, source) => {
   c.moveTo(0, 0)
   c.stroke()
   c.restore()
+  if (source) {
+    renderSourceMarker(x, y, orientation)
+  }
 }
 
 const renderLambda = (x, y, orientation, activated, source) => {
@@ -220,6 +330,9 @@ const renderLambda = (x, y, orientation, activated, source) => {
   c.lineTo(0, cos)
   c.stroke()
   c.restore()
+  if (source) {
+    renderSourceMarker(x, y, orientation)
+  }
 }
 
 const renderLambdaFlipped = (x, y, orientation, activated, source) => {
@@ -237,6 +350,9 @@ const renderLambdaFlipped = (x, y, orientation, activated, source) => {
   c.lineTo(0, cos)
   c.stroke()
   c.restore()
+  if (source) {
+    renderSourceMarker(x, y, orientation)
+  }
 }
 
 const renderPsi = (x, y, orientation, activated, source) => {
@@ -262,6 +378,9 @@ const renderPsi = (x, y, orientation, activated, source) => {
   c.lineTo(0, cos)
   c.stroke()
   c.restore()
+  if (source) {
+    renderSourceMarker(x, y, orientation)
+  }
 }
 
 const renderChi = (x, y, orientation, activated, source) => {
@@ -281,13 +400,42 @@ const renderChi = (x, y, orientation, activated, source) => {
   c.lineTo(0, -cos)
   c.stroke()
   c.restore()
+  if (source) {
+    renderSourceMarker(x, y, orientation)
+  }
 }
+
+const renderK = (x, y, orientation, activated, source) => {
+  alert('K piece - TO DO!')
+}
+
+const renderSourceMarker = (x, y, orientation, special = false) => {
+  c.save()
+  if (special) {
+    const cos = Math.floor(
+      20 * Math.cos((Math.PI / 6) * ((orientation + 1) % 6))
+    )
+    const sin = Math.floor(
+      20 * Math.sin((Math.PI / 6) * ((orientation + 1) % 6))
+    )
+    c.translate(x + sin, y + cos)
+    //c.rotate((Math.PI / 3) * orientation + Math.PI / 4)
+  } else {
+    c.translate(x, y)
+    c.rotate((Math.PI / 3) * orientation + Math.PI / 4)
+    c.fillStyle = COLORS.SOURCE
+  }
+  c.fillRect(-5, -5, 10, 10)
+  c.restore()
+}
+
+/* tests
 
 const testRenderConnectors = (row, f) => {
   let x, y
   for (let col = 0; col < 6; col++) {
     [x, y] = hexIndexToCenterCoords(row, col)
-    f(x + HEX_X0, y + HEX_Y0, col, col === 2 || col === 4, col === 4)
+    f(x + HEX_X0, y + HEX_Y0, col, col === 2 || col === 4, true)
   }
 }
 
@@ -301,3 +449,20 @@ testRenderConnectors(6, renderLambda)
 testRenderConnectors(7, renderLambdaFlipped)
 testRenderConnectors(8, renderPsi)
 testRenderConnectors(9, renderChi)
+testRenderConnectors(10, renderK)
+
+
+let f1 = subscribeToCanvasClick(ev => {
+  console.log('f1')
+})
+let f2 = subscribeToCanvasClick(ev => {
+  console.log('f2')
+})
+let f3 = subscribeToCanvasClick(ev => {
+  console.log('f3')
+  f2()
+})
+
+// first click will log f1, f2, f3
+// successive clicks will log f1,f3 (f2 having been removed, and calling f2() multiple times is a no-op)
+*/
